470e35fb47bd2e6d8a879adadf2ab41158575052
wuyx <1007178106@qq.com>
2020-08-17 17:14:16 +0800

    创建工程，开始深入学习Spring

---
+++ spring-ioc/bean-resolve-register/src/main/java/cn/tuacy/spring/bean/resolve/register/ClassPathXmlResolveRegisterApplication.java
@@ -0,0 +1,17 @@
+package cn.tuacy.spring.bean.resolve.register;
+
+/**
+ *
+ * XML配置文件的方式
+ *
+ * @version 1.0
+ * @author: tuacy.
+ * @date: 2020/8/17 14:05.
+ */
+public class ClassPathXmlResolveRegisterApplication {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ spring-ioc/bean-resolve-register/src/main/java/cn/tuacy/spring/bean/resolve/register/entity/User.java
@@ -0,0 +1,28 @@
+package cn.tuacy.spring.bean.resolve.register.entity;
+
+/**
+ * @version 1.0
+ * @author: tuacy.
+ * @date: 2020/8/17 14:07.
+ */
+public class User {
+
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
